4b334c1f48be6c28c39b410f49d7efbb7ae7d9ce
krishna_dixit <rkdixit96@gmail.com>
2018-01-16 10:00:51 +0530

    Basic functionality

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,17 @@
 function upperCaser(input) {
-	return null;
+	if(typeof input === "string"){
+		return input.toUpperCase();
+	}
+	else{
+		//pass
+	}
+	
 }
 
 
 function testing(){
 	console.log("Basic functionality:", upperCaser("hello")==="HELLO");
+	console.log("Wrong input type:", upperCaser(1)===null);
 }
 
 testing()
