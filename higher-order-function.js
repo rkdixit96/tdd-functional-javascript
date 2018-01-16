51e0572a0e0c55c15eca5c169fe9ae0ca64c3598
krishna_dixit <rkdixit96@gmail.com>
2018-01-16 11:34:49 +0530

    Higher order function- Initial commit

---
+++ higher-order-function.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+function repeat(operation, num){
+	counter()
+}
+
+function counter(){
+	count++;
+}
+
+function resetCounter(){
+	count = 0
+}
+
+function testing(){
+	let num = 5;
+	repeat(operation,num);
+	console.log("Basic functionality:",count === num);
+}
+
+function operation(){
+	//pass
+}
+
+testing();
