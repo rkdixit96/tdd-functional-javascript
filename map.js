c699856930d19e1bb0ca6b7b52e99a21d38ca1db
krishna_dixit <rkdixit96@gmail.com>
2018-01-16 12:36:36 +0530

    Map -Basic functionality with test for incorrect answer and empty input

--- map.js
+++ map.js
@@ -1,9 +1,24 @@
 function doubleAll(numbers){
+	return numbers.map(x => x * 2);
 
 }
 
 function testing(){
-	console.log("Basic functionality:", doubleAll([1,2,3])===[2,4,6])
+	console.log("Basic functionality:", isEqual(doubleAll([1,2,3]),[2,4,6]));
+	console.log("Incorrect answer as incorrect:", !isEqual(doubleAll([1,2,3]),[2,5,6]));
+	console.log("Empty array:", isEqual(doubleAll([]),[]));
+}
+
+function isEqual(arr1,arr2){
+	for(let i = 0; i < arr1.length; i++){
+		if(arr1[i] === arr2[i]){
+			//pass
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 
 testing()
