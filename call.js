cef07d7d2d54cbdaddf19f977041aa83495e5745
krishna_dixit <rkdixit96@gmail.com>
2018-01-17 14:13:18 +0530

    Call- Basic functionality, negative test case, no parameter with property, function with no params

--- call.js
+++ call.js
@@ -1,11 +1,25 @@
 function duckCount(){
-
+	//call is used to get access to functions/objects of other objects
+	console.log(arguments)
+	var arr = [].slice.call(arguments);
+	console.log(arr);
+	//console.log(typeof arr)
+	return arr.reduce(function counter (nDucks, obj){
+		if(Object.prototype.hasOwnProperty.call(obj,'quack')){
+			nDucks += 1;
+		}
+		return nDucks;
+	},0);
 
 }
 
 function testing(){
 
-	console.log("Basic functionality:", duckCount({quack: true}) === 1)
+	console.log("Basic functionality:", duckCount({quack: true}) === 1);
+	console.log("Negative case:", duckCount({quack:true},{quack:true}) != 1);
+	console.log("No parameter that has property:", duckCount({notquack: false}) === 0);
+	console.log("Function has no parameters:",duckCount()===0);
+
 }
 
 testing();
