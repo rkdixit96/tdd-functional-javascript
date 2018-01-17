dd90932783f5d4406b1e2468ccbf48093895e9ef
krishna_dixit <rkdixit96@gmail.com>
2018-01-17 08:01:21 +0530

    Reduce- Initial commit

---
+++ reduce.js
@@ -0,0 +1,13 @@
+function countWords(inputWords){
+
+}
+
+function testing(){
+	console.log("Basic functionality:",countWords(['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian'])==={
+       Apple: 2,
+       Banana: 1,
+       Durian: 3
+    })
+}
+
+testing()
